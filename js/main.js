be10b291a2838b42cbdff80ed7ed8532005eae12
Dominic Serrano <dmsrojo@gmail.com>
2017-01-17 11:56:00 -0600

    Rendering html with React.createElement

---
+++ js/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const numList = [
+  { name: "Daniel", age: 40 },
+  { name: "Scott", age: 41 },
+  { name: "Greg", age: 30 }
+];
+
+// Iterate over array and create li's dynamically using react.createElement
+const generateItemList = (list) => (
+  list.map((each, i) => {
+    let text = `Name: ${each.name}, Age: ${each.age}`;
+    return React.createElement('li', { key: i }, text)
+  })
+);
+
+// Create a root element using React.createElement
+// React.createElement('tagname', { pass in attrs, ie: className: 'main' }, 'html content')
+const rootElement =
+  React.createElement('div', {},
+    React.createElement('h1', {}, 'My List'),
+    React.createElement('ul', {}, generateItemList(numList))
+  );
+
+// Render rootElement to page
+ReactDOM.render(rootElement, document.getElementById('react-app'));
