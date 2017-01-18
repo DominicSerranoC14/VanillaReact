e90302700fdc599f4fecd7866bde76e32be6a12e
Dominic Serrano <dmsrojo@gmail.com>
2017-01-17 20:36:21 -0600

    Contact form class is now rendering correctly

--- js/createClass.js
+++ js/createClass.js
@@ -6,14 +6,12 @@ const numList = [
   { id: 3, name: "Greg", age: 30 }
 ];
 
-
+// ListItem class which creates a contact element from an array of contact obj's
 const ListItem = React.createClass({
-
   propTypes: {
     name: React.PropTypes.string,
     age: React.PropTypes.number
   },
-
   render: function() {
     return (
       React.createElement('div', {},
@@ -21,15 +19,42 @@ const ListItem = React.createClass({
       )
     )
   }
-
 });
 
+// Empty contact object to pass to input form as prop
+const newContact = { name: "", age: "", description: "" }
+
+// ContactForm Class which renders a new contact form
+const ContactForm = React.createClass({
+  propTypes: {
+    contact: React.PropTypes.object.isRequired
+  },
+  render: function() {
+    return (
+      React.createElement('form', {},
+        React.createElement('input', {
+          type: 'text', placeholder: 'Name', value: this.props.contact.name
+        }),
+        React.createElement('input', {
+          type: 'text', placeholder: 'Age', value: this.props.contact.age
+        }),
+        React.createElement('textarea', {
+          placeholder: 'Description', value: this.props.contact.description}),
+        React.createElement('button', {type: 'submit'}, 'Submit')
+      )
+    )
+  }
+});
+
+// Returns an array of React elements using ListItem class
 const contactList = numList.map(each => React.createElement(ListItem, each));
 
+// React elements to output
 const rootElement =
   React.createElement('div', {},
     React.createElement('h1', {}, 'Contacts'),
-    React.createElement('div', {}, contactList)
+    React.createElement('div', {}, contactList),
+    React.createElement(ContactForm, {contact: newContact})
   );
 
 // Render rootElement to page
